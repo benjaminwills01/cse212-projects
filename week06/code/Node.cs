48b235b6c2682737f58f7e9b77ff9fa5ec047434
benjaminwills01 <you@example.com>
2025-02-14 13:12:38 +0200

    changes

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,21 +1,22 @@
 public class Node
 {
     public int Data { get; set; }
-    public Node? Right { get; private set; }
-    public Node? Left { get; private set; }
+    public Node? Left { get; set; }
+    public Node? Right { get; set; }
 
     public Node(int data)
     {
-        this.Data = data;
+        Data = data;
     }
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
-        if (value < Data)
+        if (value == Data)
         {
-            // Insert to the left
+            return; // Prevent duplicate values
+        }
+        else if (value < Data)
+        {
             if (Left is null)
                 Left = new Node(value);
             else
@@ -23,7 +24,6 @@ public class Node
         }
         else
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
@@ -33,13 +33,25 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data && Left != null)
+        {
+            return Left.Contains(value);
+        }
+        else if (value > Data && Right != null)
+        {
+            return Right.Contains(value);
+        }
         return false;
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
